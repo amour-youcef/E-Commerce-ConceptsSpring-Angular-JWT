29cefa47e6d1bdd64d0ba6c86ee5ee745b2ef220
dev <amour-youcef@hotmail.com>
2020-05-15 09:10:28 +0200

    15052020

--- ecom-web/src/app/app.component.ts
+++ ecom-web/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CatalogueService } from './catelogue.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ecom-web';
+  public categories;
+  constructor(private catService: CatalogueService) {
+
+  }
+  ngOnInit(): void {
+    this.getCategories();
+  }
+  private getCategories() {
+    this.catService.getResource("/categories").subscribe(data => {
+      this.categories = data;
+    }, err => {
+      console.log(err);
+    })
+  }
+
 }
