a49670e418773c851130bce78d53b8222c19dcbd
dev <amour-youcef@hotmail.com>
2020-05-17 20:32:06 +0200

    17052020

--- e-commerce-backend-spring/src/main/java/org/sid/lightecomv1/LightEcomV1Application.java
+++ e-commerce-backend-spring/src/main/java/org/sid/lightecomv1/LightEcomV1Application.java
@@ -9,14 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 
 import java.util.Random;
 
 @SpringBootApplication
-public class LightEcomV1Application extends SpringBootServletInitializer {
+public class LightEcomV1Application implements CommandLineRunner {
     @Autowired
     private ProductRepository productRepository;
     @Autowired
@@ -27,33 +25,28 @@ public class LightEcomV1Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(LightEcomV1Application.class, args);
     }
-    
-    
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(LightEcomV1Application.class);
-    }
 
+    @Override
     public void run(String... args) throws Exception {
 
-        repositoryRestConfiguration.exposeIdsFor(Product.class,Category.class);
-
-        categoryRepository.save(new Category(null,"Computers",null,null,null));
-        categoryRepository.save(new Category(null,"Printers",null,null,null));
-        categoryRepository.save(new Category(null,"Smart phones",null,null,null));
-        Random rnd=new Random();
-        categoryRepository.findAll().forEach(c->{
-            for (int i = 0; i <10 ; i++) {
-                Product p=new Product();
-                p.setName(RandomString.make(18));
-                p.setCurrentPrice(100+rnd.nextInt(10000));
-                p.setAvailable(rnd.nextBoolean());
-                p.setPromotion(rnd.nextBoolean());
-                p.setSelected(rnd.nextBoolean());
-                p.setCategory(c);
-                p.setPhotoName("img01.png");
-                productRepository.save(p);
-            }
-        });
+//        repositoryRestConfiguration.exposeIdsFor(Product.class,Category.class);
+//
+//        categoryRepository.save(new Category(null,"Computers",null,null,null));
+//        categoryRepository.save(new Category(null,"Printers",null,null,null));
+//        categoryRepository.save(new Category(null,"Smart phones",null,null,null));
+//        Random rnd=new Random();
+//        categoryRepository.findAll().forEach(c->{
+//            for (int i = 0; i <10 ; i++) {
+//                Product p=new Product();
+//                p.setName(RandomString.make(18));
+//                p.setCurrentPrice(100+rnd.nextInt(10000));
+//                p.setAvailable(rnd.nextBoolean());
+//                p.setPromotion(rnd.nextBoolean());
+//                p.setSelected(rnd.nextBoolean());
+//                p.setCategory(c);
+//                p.setPhotoName("unknown.png");
+//                productRepository.save(p);
+//            }
+//        });
     }
 }
